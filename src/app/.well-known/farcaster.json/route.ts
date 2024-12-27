7cf3867a4fcd56a801597b7474fcacb55cfcfb02
KITE <whoskitemedia@gmail.com>
2024-12-26 19:28:12 -0800

    AA Update

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -5,20 +5,20 @@ export async function GET() {
     accountAssociation: {
       header:
         "eyJmaWQiOjE1NDA5LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4NjA5NTU5MDYyMUE4YUZGM2ZjOTY0QjEwYzMzN2EzNzVjNTI3OTI0OCJ9",
-      payload: "eyJkb21haW4iOiJxZy1mcmFtZXMtMnRobnRoMjRxLWtpdGVzLXByb2plY3RzLTM4YWJlZGI5LnZlcmNlbC5hcHAifQ",
+      payload: "eyJkb21haW4iOiJxZy1mcmFtZXMudmVyY2VsLmFwcCJ9",
       signature:
-        "MHhmYjRhNmY1YmEzMWMwNzBmNmM3OWUxN2I3OTJlMWEwYTcwMGIwNzI4OGYzODFmOWMxZTA0YmZjMWYzNzg0ZGEwMWM2NDNjNGYzZTM2N2MwMDg3Yjc4ZGE5OTQ3ODc2NDlmZjAzY2IxNjkwMmQxZmRiM2U0YmRhY2MxNjFjYmU1NDFi",
+        "MHg2NzlmYmRkMGRiODExNTY2OTg0ZDRkYWM1MDBkMTAxYzNjZmRhMzQ5MDM5YzZjMDZlNTgzZmFhZmM3ODgyNDgwMWY0MDNkYTJkODkxY2ExNzQ1MmUwNDY5NjE1M2IxZjE3YzU3NjgyZmVhOWMwNjZlNWI5NzRkMjU0ZTMzMmI3YjFj",
     },
     frame: {
       version: "1",
       name: "Quote Generator",
-      homeUrl: "https://qg-frames.vercel.app//",
-      iconUrl: `https://qg-frames.vercel.app//icon.png`,
-      imageUrl: `https://qg-frames.vercel.app//image.png`,
+      homeUrl: "https://qg-frames.vercel.app/",
+      iconUrl: `https://qg-frames.vercel.app/icon.png`,
+      imageUrl: `https://qg-frames.vercel.app/image.png`,
       buttonTitle: "Launch Frame",
-      splashImageUrl: "https://qg-frames.vercel.app//splash.png",
+      splashImageUrl: "https://qg-frames.vercel.app/splash.png",
       splashBackgroundColor: "#f7f7f7",
-      webhookUrl: "https://qg-frames.vercel.app//api/webhook",
+      webhookUrl: "https://qg-frames.vercel.app/api/webhook",
     },
   };
 
