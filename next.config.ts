7bddd03161d9ed8c4b14fafaf5009ea2e24388b3
KITE <whoskitemedia@gmail.com>
2024-12-26 19:56:03 -0800

    Update Next.Config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const appUrl = "https://example.com"; // Replace with your actual URL
+const appUrl = "https://qg-frames.vercel.app/"; // Replace with your actual URL
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['media1.giphy.com', 'media3.giphy.com', 'media2.giphy.com', 'media4.giphy.com', 'media0.giphy.com', 'https://qg-frames-3i5jowxji-kites-projects-38abedb9.vercel.app/'],
+    domains: ['media1.giphy.com', 'media3.giphy.com', 'media2.giphy.com', 'media4.giphy.com', 'media0.giphy.com', 'https://qg-frames.vercel.app/'],
   }
 };
 
