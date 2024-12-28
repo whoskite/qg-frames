ed85a2f114dea500bca2633e0a89aa56ec8dc2d1
KITE <whoskitemedia@gmail.com>
2024-12-27 18:15:09 -0800

    fix: shareText

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -620,7 +620,7 @@ export default function Demo(
           >
             <Button 
               onClick={() => {
-                const shareText = 'Create "Fun Quotes" by @KITE and /thepod team 🃏';
+                const shareText = 'Create "Fun Quotes" by @KITE and /thepod team 👽';
                 const shareUrl = 'https://qg-frames.vercel.app';
                 const url = `https://warpcast.com/~/compose?text=${encodeURIComponent(shareText)}&embeds[]=${encodeURIComponent(shareUrl)}`;
                 sdk.actions.openUrl(url);
