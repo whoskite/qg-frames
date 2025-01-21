9b3d53b9d25418b24aeb637f5c6597f7ef2c0f94
KITE <whoskitemedia@gmail.com>
2025-01-21 07:40:03 -0800

    Update Content Security Policy in next.config.js to enhance security and analytics tracking. Added Google Tag Manager and Google Analytics sources to script, img, connect, and frame directives for improved data collection and monitoring.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https://*.vercel.app https://funquotes-test.vercel.app https://cdn.ngrok.com; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' data: https://fonts.gstatic.com https://*.ngrok.com https://assets.ngrok.com https://cdn.ngrok.com; img-src 'self' data: https: blob:; connect-src 'self' data: https://*.vercel.app https://funquotes-test.vercel.app https://*.firebaseio.com https://*.googleapis.com https://*.ngrok.com; frame-src 'self' https://*.vercel.app https://funquotes-test.vercel.app; media-src 'self' https://*.vercel.app https://funquotes-test.vercel.app;"
+            value: "default-src 'self' https://*.vercel.app https://funquotes-test.vercel.app https://cdn.ngrok.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com; style-src 'self' 'unsafe-inline'; font-src 'self' data: https://fonts.gstatic.com https://*.ngrok.com https://assets.ngrok.com https://cdn.ngrok.com; img-src 'self' data: https: blob: https://*.google-analytics.com https://www.googletagmanager.com; connect-src 'self' data: https://*.vercel.app https://funquotes-test.vercel.app https://*.firebaseio.com https://*.googleapis.com https://*.ngrok.com https://*.google-analytics.com https://www.googletagmanager.com https://analytics.google.com; frame-src 'self' https://*.vercel.app https://funquotes-test.vercel.app; media-src 'self' https://*.vercel.app https://funquotes-test.vercel.app;"
           }
         ]
       }
