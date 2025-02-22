ef9084d6b0a3aca6ddd03b56b3e36b3d77a286b9
KITE <whoskitemedia@gmail.com>
2025-02-21 17:43:27 -0800

    Update Meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,10 @@ export const revalidate = 300;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Farcaster Frames v2 Demo",
+    title: "FunQuotes",
     openGraph: {
-      title: "Farcaster Frames v2 Demo",
-      description: "A Farcaster Frames v2 demo app.",
+      title: "FunQuotes",
+      description: "Create and share quotes with your friends.",
     },
     other: {
       "fc:frame": JSON.stringify(frame),
